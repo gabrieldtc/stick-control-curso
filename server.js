17bd81c1c9b116e257aa853e2c311481434086dc
Gabriel <gabrieldtc@hotmail.com>
2026-08-30 18:59:03 -0300

    Fix: Serve static files from public folder

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ const DB_PATH = process.env.DB_PATH ? path.join(process.env.DB_PATH, 'travesseir
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 let db;
